addfb00266b2ba1f9fe58aa4b1d92140b90b6056
danielvazquez-utp <daniel.vazquez@utpuebla.edu.mx>
2022-10-19 22:16:12 -0500

    useEffect implementado

--- src/screens/WeatherScreen.js
+++ src/screens/WeatherScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
 
 import { FontAwesome5 } from '@expo/vector-icons';
@@ -10,6 +10,31 @@ const WeatherScreen = () => {
   const [temperatura, setTemperatura] = useState(20);
   const [clima, setClima] = useState('Parcialmente nublado');
   const [vientovel, setVientovel] = useState(17);
+  const [icono, setIcono] = useState('//cdn.weatherapi.com/weather/64x64/night/116.png');
+
+  
+  const getWeatherAsync = async () => {
+    try {
+      const apiKey = '7bd489b59b7847d8bcf191838221910';
+      const response = await fetch(`https://api.weatherapi.com/v1/current.json?q=${ ciudad }&key=${ apiKey }&lang=es`);
+      const { current, location } = await response.json();
+      // const { url } = data.images.original;
+      // setUrlImage(url);
+      setPais( location.country );
+      setTemperatura( current.temp_c );
+      setClima( current.condition.text );
+      setIcono( current.condition.icon );
+      setVientovel( current.wind_mph );
+      setCiudad( location.name );
+    } catch (error) {
+        Alert.alert(error);
+    }
+  }
+
+  useEffect(() => {
+    // Realiza peticion a weatherapi.com sobre el clima en tiempo real
+    getWeatherAsync();
+  }, []);
     
   return (
     <ImageBackground 
@@ -19,17 +44,17 @@ const WeatherScreen = () => {
       <Text style={ styles.ciudad }>{ ciudad }, { pais }</Text>
       <Text style={ styles.temperatura }>{ temperatura }°c</Text>
       <Image
-        source={{ uri: 'https://cdn.weatherapi.com/weather/64x64/day/116.png' }}
+        source={{ uri: `https:${ icono }` }}
         style={ styles.imagen }
         />
-      <Text>{ clima }</Text>
+      <Text style={ styles.clima } >{ clima }</Text>
       <FontAwesome5 
         name="wind" 
-        size={24} 
+        size={32} 
         color="white" 
         style={ styles.viento }
       />
-      <Text>{ vientovel }Km/h</Text>
+      <Text style={ styles.clima } >{ vientovel }Km/h</Text>
     </ImageBackground>
   )
 }
@@ -44,7 +69,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   'temperatura': {
-    fontSize: 100,
+    fontSize: 120,
     color: 'white',
   },
   'ciudad' : {
@@ -53,12 +78,16 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   'imagen' : {
-    width: 50,
-    height: 50,
+    width: 75,
+    height: 75,
     marginBottom: 10,
   },
   'viento' : {
     marginBottom: 10,
     marginTop: 10,
+    fontWeight: 'bold',
+  },
+  'clima' : {
+    fontWeight: 'bold',
   }
 })
